3bf63db88de08ba0059b51b1df83fc247a9d09b6
ljwoon1211 <ljwoon1211@gmail.com>
2025-06-04 23:59:19 +0900

    fix: main.js setTimeout 대신 css로 변경.firebase 캐싱 옵션

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
-function showTopBar(){
-    let country = "France";
-    let vat = 20;
-    setTimeout(() => {
-        document.querySelector("section.country-bar")
-        .innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`
-        document.querySelector("section.country-bar")
-        .classList.remove('hidden')
-    }, 1000);
+function showTopBarWithAnimation() {
+  const country = "France";
+  const vat = 20;
+  const countryBarElement = document.querySelector("section.country-bar");
+
+  if (countryBarElement) {
+    countryBarElement.innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`;
+    if (countryBarElement.classList.contains('hidden')) {
+      countryBarElement.classList.remove('hidden');
+    }
+    countryBarElement.classList.add('visible-with-css-delay');
+  }
 }
 
-
-
-showTopBar();
-
+// DOM이 완전히 로드된 후 함수를 실행합니다.
+document.addEventListener('DOMContentLoaded', showTopBarWithAnimation);
 
 
 
