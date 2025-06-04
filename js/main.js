a0bccabb43cd0ac9485359e8e606b751f847b231
ljwoon1211 <ljwoon1211@gmail.com>
2025-06-05 01:11:49 +0900

    feat: defer 추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// js/main.js 파일 내 수정 제안
 function showTopBarWithAnimation() {
   const country = "France";
   const vat = 20;
@@ -5,15 +6,9 @@ function showTopBarWithAnimation() {
 
   if (countryBarElement) {
     countryBarElement.innerHTML = `<p>Orders to <b>${country}</b> are subject to <b>${vat}%</b> VAT</p>`;
-    if (countryBarElement.classList.contains('hidden')) {
-      countryBarElement.classList.remove('hidden');
-    }
     countryBarElement.classList.add('visible-with-css-delay');
   }
 }
 
 // DOM이 완전히 로드된 후 함수를 실행합니다.
 document.addEventListener('DOMContentLoaded', showTopBarWithAnimation);
-
-
-
